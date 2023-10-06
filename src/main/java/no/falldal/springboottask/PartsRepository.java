24e8335e8577a113cb4d0422ddff022d84baa504
Vegard Falldalen <vegfall@gmail.com>
2023-10-06 17:54:33 +0200

    Added add, delete, get and change method for repository. Added tests for repository

--- src/main/java/no/falldal/springboottask/PartsRepository.java
+++ src/main/java/no/falldal/springboottask/PartsRepository.java
@@ -1,7 +1,46 @@
 package no.falldal.springboottask;
 
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
 @Component
+@Getter
 public class PartsRepository {
+    private final ArrayList<Part> partsList;
+
+    public PartsRepository() {
+        partsList = new ArrayList<>();
+    }
+
+    public void addPart(Part part) {
+        partsList.add(part);
+    }
+
+    public Part getPart(Part part) {
+        if (partsList.contains(part)) {
+            return part;
+        }
+
+        return null;
+    }
+
+    public void updatePart(Part oldPart, Part newPart) {
+        for (int i = 0; i < partsList.size(); i++) {
+            if (partsList.get(i).equals(oldPart)) {
+                partsList.set(i, newPart);
+                break;
+            }
+        }
+    }
+
+    public void deletePart(Part part) {
+        for (int i = 0; i < partsList.size(); i++) {
+            if (partsList.get(i).equals(part)) {
+                partsList.remove(part);
+            }
+        }
+    }
 }
---
+++ src/test/java/no/falldal/springboottask/PartsRepositoryUnitTest.java
@@ -0,0 +1,41 @@
+package no.falldal.springboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PartsRepositoryUnitTest {
+    @MockBean
+    PartsRepository partsRepository = new PartsRepository();
+
+    @Test
+    void testRepositoryAddPart() {
+        partsRepository.addPart(new Part("Gear", 1));
+        assertEquals(1, partsRepository.getPartsList().size());
+        assertEquals("Gear", partsRepository.getPartsList().get(0).name());
+    }
+
+    @Test
+    void testRepositoryGetSinglePart() {
+        partsRepository.addPart(new Part("Screw", 2));
+        partsRepository.addPart(new Part("Nail", 3));
+        assertEquals("Nail", partsRepository.getPart(new Part("Nail", 3)).name());
+    }
+
+    @Test
+    void testRepositoryUpdatePart() {
+        partsRepository.addPart(new Part("Cog", 4));
+        partsRepository.updatePart(new Part("Cog", 4), new Part("Cog", 5));
+        assertEquals(1, partsRepository.getPartsList().size());
+        assertEquals(5, partsRepository.getPartsList().get(0).price());
+    }
+
+    @Test
+    void testRepositoryDeletePart() {
+        partsRepository.addPart(new Part("Spring", 6));
+        assertEquals(1, partsRepository.getPartsList().size());
+        partsRepository.deletePart(new Part("Spring", 6));
+        assertEquals(0, partsRepository.getPartsList().size());
+    }
+}
